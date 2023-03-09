8bd126ce7e6fde600b81d7a63db7c0ac78386172
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-03-09 23:27:48 +0200

    Task2 : Payment Entry - naming_series=GSG-JV-.YYYY.- (Comment)

--- erpnext14_gsg/public/js/payment_entry.js
+++ erpnext14_gsg/public/js/payment_entry.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on('Payment Entry', {
 	refresh(frm) {
 		// your code here
-		msgprint('bbbbbb');
+//		msgprint('bbbbbb');
         var new_option = ["GSG-JV-.YYYY.-"]
 
 		set_field_options("naming_series", new_option);
