485f7ed04a232af1c83a945ea2e10bd9207452fc
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2023-03-09 22:42:56 +0200

    Task1 : journal_entry new_options ( remove 2 options)

--- erpnext14_gsg/public/js/journal_entry.js
+++ erpnext14_gsg/public/js/journal_entry.js
@@ -1,13 +1,24 @@
 frappe.ui.form.on('Journal Entry', {
 	refresh(frm) {
 		// your code here
-		msgprint('ccccc');
-        var old_options = frm.doc.voucher_type
-        console.log('11111');
-        console.log(old_options);
-        console.log('2222222');
+//		msgprint('bbbbbb');
+        var new_options = ["Journal Entry",
+                            "Bank Entry",
+                            "Cash Entry",
+                            "Credit Card Entry",
+                            "Debit Note",
+                            "Credit Note",
+                            "Contra Entry",
+                            "Excise Entry",
+                            "Write Off Entry",
+                            "Opening Entry",
+                            "Depreciation Entry",
+                            "Exchange Rate Revaluation",
+                            "Exchange Gain Or Loss",
+                            "Deferred Revenue"
+                            ]
 
-		set_field_options("voucher_type", ["help_content", "zaaaaaaaak"]);
+		set_field_options("voucher_type", new_options);
 
 
 
